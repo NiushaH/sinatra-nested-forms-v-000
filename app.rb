fefe0189b16d7a9e51d4501ba0ad9682413ac065
Niusha H <niusha.hadziomerovic@gmail.com>
2020-02-03 02:15:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,15 @@ module FormsLab
     end
 
     post '/pirates' do
-      ""
+      # @pirate = Pirate.new(params[:pirate])
+
+      # params[:pirate][:ships].each do |args|
+      #   Ship.new(args)
+      # end
+
+      # @ships = Ship.all
+
+      erb :'/pirates/show'
     end    
   end
 end
